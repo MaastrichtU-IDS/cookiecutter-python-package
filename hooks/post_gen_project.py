5612866669d3db2c2e2d529b7e55d017202c7489
Vincent Emonet <vincent.emonet@gmail.com>
2022-07-27 19:17:22 +0200

    param for docs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,10 +7,12 @@ import pathlib
 
 PROJECT_DIRECTORY = pathlib.Path(os.path.realpath(os.path.curdir)).resolve()
 PACKAGE = PROJECT_DIRECTORY.joinpath("src", "{{ cookiecutter.package_name }}")
-DOCS = PROJECT_DIRECTORY.joinpath("docs", "source")
+DOCS = PROJECT_DIRECTORY.joinpath("docs")
 
 if __name__ == '__main__':
     if '{{ cookiecutter.command_line_interface|lower }}' == "false":
-        PACKAGE.joinpath("cli.py").unlink()
         PACKAGE.joinpath("__main__.py").unlink()
-        DOCS.joinpath("cli.rst").unlink()
+
+    if '{{ cookiecutter.documentation_website|lower }}' == "false":
+        PROJECT_DIRECTORY.joinpath("mkdocs.yml").unlink()
+        DOCS.unlink()
